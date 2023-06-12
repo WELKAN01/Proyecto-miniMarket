0a1cd1c8ad381adf3fb5b115696af53dd664ee6a
andy daniel leon curo <andy_leon_16@hotmail.com>
2023-06-12 00:57:09 -0500

    12/06/2023 animation login

--- app/src/main/java/com/example/proyectominimarket/MainActivity.java
+++ app/src/main/java/com/example/proyectominimarket/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.annotation.SuppressLint;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,7 +18,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button btningresar;
     TextView bienvenido;
-    ConstraintLayout l;
     boolean i=true;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,17 +27,29 @@ public class MainActivity extends AppCompatActivity {
         //l=findViewById(androidx.constraintlayout.widget.R.id.constraint);
         btningresar=findViewById(R.id.btnIngreso);
         bienvenido=findViewById(R.id.txvBienvenido);
+        ConstraintLayout constra=(ConstraintLayout) findViewById(R.id.constraintLayout);
+        ConstraintLayout constralogin=(ConstraintLayout) findViewById(R.id.constraintlogin);
+        AnimationDrawable animationDrawable=(AnimationDrawable) constra.getBackground();
+
         btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(i){
                     bienvenido.setVisibility(v.INVISIBLE);
-                    Intent intent=new Intent(v.getContext(), dashboardActivity.class);
-                    startActivity(intent);
+                    animationDrawable.setOneShot(true);
+                    animationDrawable.setEnterFadeDuration(500);
+                    animationDrawable.start();
+                    try {
+                        Thread.sleep(1000);
+                        constralogin.setVisibility(v.VISIBLE);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    //Intent intent=new Intent(v.getContext(), dashboardActivity.class);
+                    //startActivity(intent);
                     i=false;
                 }else{
-                    bienvenido.setVisibility(v.VISIBLE);
-                    i=true;
                 }
 
             }
--- app/src/main/java/com/example/proyectominimarket/dashboardActivity.java
+++ app/src/main/java/com/example/proyectominimarket/dashboardActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -15,6 +20,8 @@ public class dashboardActivity extends AppCompatActivity {
     FragmentTransaction fragmentTransaction;
     Fragment principalfragment,searchfragment,carritofragment,perfilfragment;
     BottomNavigationView navigation;
+    TextView titulo;
+    CharSequence nameheader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,27 +30,33 @@ public class dashboardActivity extends AppCompatActivity {
         searchfragment=new Searchfragmento();
         perfilfragment=new Perfilfragmento();
         setContentView(R.layout.activity_dashboard);
+        titulo=(TextView) findViewById(R.id.txvTitulo);
         navigation=findViewById(R.id.navigationBottom);
         navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.home:
+                        titulo.setText("Categorias");
                         fragmentTransaction=getSupportFragmentManager().beginTransaction();
                         fragmentTransaction.replace(R.id.fragmentContainerView,principalfragment);
                         fragmentTransaction.addToBackStack(null).commit();
                         return true;
                     case R.id.search:
+                        titulo.setText("Busqueda");
                         fragmentTransaction=getSupportFragmentManager().beginTransaction();
                         fragmentTransaction.replace(R.id.fragmentContainerView,searchfragment);
                         fragmentTransaction.addToBackStack(null).commit();
                         return true;
                     case R.id.cart:
+                        titulo.setText("Carrito");
                         fragmentTransaction=getSupportFragmentManager().beginTransaction();
                         fragmentTransaction.replace(R.id.fragmentContainerView,carritofragment);
                         fragmentTransaction.addToBackStack(null).commit();
+
                         return true;
                     case R.id.profile:
+                        titulo.setText("Perfil");
                         fragmentTransaction=getSupportFragmentManager().beginTransaction();
                         fragmentTransaction.replace(R.id.fragmentContainerView,perfilfragment);
                         fragmentTransaction.addToBackStack(null).commit();
